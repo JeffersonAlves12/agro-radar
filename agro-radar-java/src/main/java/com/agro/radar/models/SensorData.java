bb83c09c84165bb79a44c524986a3c3b0d8ed210
Cheng <davchin93@gmail.com>
2025-02-19 09:19:56 -0300

    push e uma mudanca: Em @ManyToOne(fetch = FetchType.LAZY):
    Lazy->EAGER

--- agro-radar-java/src/main/java/com/agro/radar/models/SensorData.java
+++ agro-radar-java/src/main/java/com/agro/radar/models/SensorData.java
@@ -1,8 +1,17 @@
 package com.agro.radar.models;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "sensor_data")
@@ -19,7 +28,7 @@ public class SensorData {
     private LocalDateTime timestamp;
     
     @JsonBackReference
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "dispositivo_id")
     private Dispositivo dispositivo;
 
@@ -62,5 +71,10 @@ public class SensorData {
 
     public void setDispositivo(Dispositivo dispositivo) {
         this.dispositivo = dispositivo;
+    }
+
+    // Método personalizado para acessar o nome do dispositivo
+    public String getDispositivoNome() {
+        return dispositivo != null ? dispositivo.getNome() : null;
     }
 }
